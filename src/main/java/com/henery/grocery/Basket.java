c8a42716b279d2a41642b29dda73f035acb28f12
Mido <mahmoud.ali@devsquads.com>
2021-11-01 23:56:14 +0200

    modify access modifiers to be private in Basket Class

--- src/main/java/com/henery/grocery/Basket.java
+++ src/main/java/com/henery/grocery/Basket.java
@@ -4,15 +4,15 @@ import java.time.LocalDate;
 
 public class Basket {
     public static final LocalDate TODAY = LocalDate.now();
-    public static final double SOUP_UNIT_PRICE = 0.65;
-    public static final double BREAD_UNIT_PRICE = 0.80;
-    public static final double APPLE_UNIT_PRICE = 0.10;
-    public static final double MILK_UNIT_PRICE = 1.30;
+    private static final double SOUP_UNIT_PRICE = 0.65;
+    private static final double BREAD_UNIT_PRICE = 0.80;
+    private static final double APPLE_UNIT_PRICE = 0.10;
+    private static final double MILK_UNIT_PRICE = 1.30;
 
     public double totalPrice(int tinsOfSoup, int loavesOfBread, int numberOfApples, int bottleOfMilk, LocalDate orderDate) {
-        double disCountValue = calculateDiscount(tinsOfSoup, orderDate);
+        double discountValue = calculateDiscount(tinsOfSoup, orderDate);
         double originalTotalPrice = calculateOriginalTotalPrice(tinsOfSoup, loavesOfBread, numberOfApples, bottleOfMilk);
-        return originalTotalPrice - disCountValue;
+        return originalTotalPrice - discountValue;
     }
 
     private double calculateDiscount(int tinsOfSoup, LocalDate orderDate) {
