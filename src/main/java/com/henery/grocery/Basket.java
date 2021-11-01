a373680ebe647adb0e98f48993d4c41406b829d0
Mido <mahmoud.ali@devsquads.com>
2021-11-01 23:50:14 +0200

    refactor Basket class to improve code quality and scannability

--- src/main/java/com/henery/grocery/Basket.java
+++ src/main/java/com/henery/grocery/Basket.java
@@ -1,13 +1,25 @@
 package com.henery.grocery;
 
+import java.time.LocalDate;
+
 public class Basket {
-    double breadDisCountValue = 0;
+    public static final LocalDate TODAY = LocalDate.now();
+    public static final double SOUP_UNIT_PRICE = 0.65;
+    public static final double BREAD_UNIT_PRICE = 0.80;
+    public static final double APPLE_UNIT_PRICE = 0.10;
+    public static final double MILK_UNIT_PRICE = 1.30;
 
-    public double totalPrice(int tinsOfSoup, int loavesOfBread, int numberOfApples, int bottleOfMilk, String today) {
-        if (today == "today" && tinsOfSoup > 1) {
-            breadDisCountValue = 0.80 / 2;
-        }
+    public double totalPrice(int tinsOfSoup, int loavesOfBread, int numberOfApples, int bottleOfMilk, LocalDate orderDate) {
+        double disCountValue = calculateDiscount(tinsOfSoup, orderDate);
+        double originalTotalPrice = calculateOriginalTotalPrice(tinsOfSoup, loavesOfBread, numberOfApples, bottleOfMilk);
+        return originalTotalPrice - disCountValue;
+    }
 
-        return (tinsOfSoup * 0.65) + (loavesOfBread * 0.80 - breadDisCountValue) + (numberOfApples * 0.10) + (bottleOfMilk * 1.30);
+    private double calculateDiscount(int tinsOfSoup, LocalDate orderDate) {
+        return (orderDate.equals(TODAY) && tinsOfSoup > 1) ? BREAD_UNIT_PRICE / 2 : 0.0;
+    }
+
+    private double calculateOriginalTotalPrice(int tinsOfSoup, int loavesOfBread, int numberOfApples, int bottleOfMilk) {
+        return (tinsOfSoup * SOUP_UNIT_PRICE) + (loavesOfBread * BREAD_UNIT_PRICE) + (numberOfApples * APPLE_UNIT_PRICE) + (bottleOfMilk * MILK_UNIT_PRICE);
     }
 }
--- src/test/java/com/henery/grocery/BasketTest.java
+++ src/test/java/com/henery/grocery/BasketTest.java
@@ -1,17 +1,30 @@
 package com.henery.grocery;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 public class BasketTest {
+
+    private static final LocalDate TODAY = LocalDate.now();
+    private static final double DELTA = 0.01;
+    private Basket basket;
+
+    @Before
+    public void setUp() {
+        basket = new Basket();
+    }
+
     @Test
     public void coastOfBasketContainingThreeTinsOfSoupTwoLoavesOfBreadBoughtToday() {
-        Assert.assertEquals(3.15, new Basket().totalPrice(3, 2, 0, 0, "today"), 0.01);
+        Assert.assertEquals(3.15, basket.totalPrice(3, 2, 0, 0, TODAY), DELTA);
 
     }
 
     @Test
     public void coastOfBasketContainingSixApplesOneBottleOfMilkBoughtToday() {
-        Assert.assertEquals(1.90, new Basket().totalPrice(0, 0, 6, 1, "today"), 0.01);
+        Assert.assertEquals(1.90, basket.totalPrice(0, 0, 6, 1, TODAY), DELTA);
     }
 }
