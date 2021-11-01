e5322119bbe4ad6638a155c354c27b4e590744fc
Mido <mahmoud.ali@devsquads.com>
2021-11-02 00:21:00 +0200

    refactor production code after adding the third flow

--- src/main/java/com/henery/grocery/Basket.java
+++ src/main/java/com/henery/grocery/Basket.java
@@ -4,27 +4,35 @@ import java.time.LocalDate;
 import java.time.temporal.TemporalAdjusters;
 
 public class Basket {
-    public static final LocalDate TODAY = LocalDate.now();
-    double appleDiscountValue = 0;
+    private static final LocalDate TODAY = LocalDate.now();
+    private static final LocalDate APPLE_DISCOUNT_END_DATE = TODAY.plusMonths(1).with(TemporalAdjusters.lastDayOfMonth());
+    private static final LocalDate APPLE_DISCOUNT_START_DATE = TODAY.plusDays(3);
     private static final double SOUP_UNIT_PRICE = 0.65;
     private static final double BREAD_UNIT_PRICE = 0.80;
     private static final double APPLE_UNIT_PRICE = 0.10;
     private static final double MILK_UNIT_PRICE = 1.30;
 
     public double totalPrice(int tinsOfSoup, int loavesOfBread, int numberOfApples, int bottleOfMilk, LocalDate orderDate) {
-        if (orderDate.isAfter(TODAY.plusDays(3)) && orderDate.isBefore(TODAY.plusMonths(1).with(TemporalAdjusters.lastDayOfMonth()))) {
-            appleDiscountValue = (numberOfApples * APPLE_UNIT_PRICE) / 10;
-        }
-        double discountValue = calculateDiscount(tinsOfSoup, orderDate);
+        double discountValue = calculateBreadDiscount(tinsOfSoup, orderDate) + calculateAppleDiscount(numberOfApples, orderDate);
         double originalTotalPrice = calculateOriginalTotalPrice(tinsOfSoup, loavesOfBread, numberOfApples, bottleOfMilk);
-        return originalTotalPrice - discountValue - appleDiscountValue;
+        return originalTotalPrice - discountValue;
     }
 
-    private double calculateDiscount(int tinsOfSoup, LocalDate orderDate) {
+    private double calculateAppleDiscount(int numberOfApples, LocalDate orderDate) {
+        return (orderDate.isAfter(APPLE_DISCOUNT_START_DATE) && orderDate.isBefore(APPLE_DISCOUNT_END_DATE))
+                ? (numberOfApples * APPLE_UNIT_PRICE) / 10
+                : 0.0;
+
+    }
+
+    private double calculateBreadDiscount(int tinsOfSoup, LocalDate orderDate) {
         return (orderDate.equals(TODAY) && tinsOfSoup > 1) ? BREAD_UNIT_PRICE / 2 : 0.0;
     }
 
     private double calculateOriginalTotalPrice(int tinsOfSoup, int loavesOfBread, int numberOfApples, int bottleOfMilk) {
-        return (tinsOfSoup * SOUP_UNIT_PRICE) + (loavesOfBread * BREAD_UNIT_PRICE) + (numberOfApples * APPLE_UNIT_PRICE) + (bottleOfMilk * MILK_UNIT_PRICE);
+        return (tinsOfSoup * SOUP_UNIT_PRICE) +
+                (loavesOfBread * BREAD_UNIT_PRICE) +
+                (numberOfApples * APPLE_UNIT_PRICE) +
+                (bottleOfMilk * MILK_UNIT_PRICE);
     }
 }
