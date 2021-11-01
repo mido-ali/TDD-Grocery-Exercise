7b15eb14ba6e5864453c74d8cd655c247431dcd6
Mido <mahmoud.ali@devsquads.com>
2021-11-01 22:58:53 +0200

    add test for 6 apples and a bottle of milk, bought today,

--- src/main/java/com/henery/grocery/Basket.java
+++ src/main/java/com/henery/grocery/Basket.java
@@ -1,7 +1,13 @@
 package com.henery.grocery;
 
 public class Basket {
-    public double totalPrice(int tinsOfSoup, int loavesOfBread, String today) {
-        return 3.15;
+    double breadDisCountValue = 0;
+
+    public double totalPrice(int tinsOfSoup, int loavesOfBread, int numberOfApples, int bottleOfMilk, String today) {
+        if (today == "today" && tinsOfSoup > 1) {
+            breadDisCountValue = 0.80 / 2;
+        }
+
+        return (tinsOfSoup * 0.65) + (loavesOfBread * 0.80 - breadDisCountValue) + (numberOfApples * 0.10) + (bottleOfMilk * 1.30);
     }
 }
--- src/test/java/com/henery/grocery/BasketTest.java
+++ src/test/java/com/henery/grocery/BasketTest.java
@@ -5,8 +5,13 @@ import org.junit.Test;
 
 public class BasketTest {
     @Test
-    public void coastOfBasketContainingThreeTinsOfSoupTwoLoavesOfBreadBoughtToday(){
-        Assert.assertEquals(3.15, new Basket().totalPrice(3, 2, "today"),0.01);
+    public void coastOfBasketContainingThreeTinsOfSoupTwoLoavesOfBreadBoughtToday() {
+        Assert.assertEquals(3.15, new Basket().totalPrice(3, 2, 0, 0, "today"), 0.01);
 
+    }
+
+    @Test
+    public void coastOfBasketContainingSixApplesOneBottleOfMilkBoughtToday() {
+        Assert.assertEquals(1.90, new Basket().totalPrice(0, 0, 6, 1, "today"), 0.01);
     }
 }
