0c98d8025dd9fdbe590488fc18df4bcd26898e7c
Mido <mahmoud.ali@devsquads.com>
2021-11-06 15:41:20 +0200

    refactor discount to be in new class

---
+++ src/main/java/com/henery/grocery/Discount.java
@@ -0,0 +1,32 @@
+package com.henery.grocery;
+
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+public class Discount {
+    static final LocalDate TODAY = LocalDate.now();
+    static final LocalDate NEXT_WEEK = TODAY.plusDays(7);
+    static final LocalDate YESTERDAY = TODAY.minusDays(1);
+    static final LocalDate APPLE_DISCOUNT_END_DATE = TODAY.plusMonths(1).with(TemporalAdjusters.lastDayOfMonth());
+    static final LocalDate APPLE_DISCOUNT_START_DATE = TODAY.plusDays(3);
+
+    public Discount() {
+    }
+
+    double calculateAppleDiscount(int numberOfApples, LocalDate orderDate) {
+        return isAppleDiscountEligible(orderDate) ? (numberOfApples * Basket.APPLE_UNIT_PRICE) / 10 : 0.0;
+
+    }
+
+    boolean isAppleDiscountEligible(LocalDate orderDate) {
+        return orderDate.isAfter(APPLE_DISCOUNT_START_DATE) && orderDate.isBefore(APPLE_DISCOUNT_END_DATE);
+    }
+
+    double calculateBreadDiscount(int tinsOfSoup, LocalDate orderDate) {
+        return isBreadDiscountEligible(tinsOfSoup, orderDate) ? Basket.BREAD_UNIT_PRICE / 2 : 0.0;
+    }
+
+    boolean isBreadDiscountEligible(int tinsOfSoup, LocalDate orderDate) {
+        return orderDate.isAfter(YESTERDAY) && orderDate.isBefore(NEXT_WEEK) && tinsOfSoup > 1;
+    }
+}
